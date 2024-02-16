9077b21a4621eca18308c072fa9ac827eedfe9ad
TheTrackLP <113531678+TheTrackLP@users.noreply.github.com>
2024-02-16 10:43:31 +0800

    Progress bar, Change config/mysql-strict = false

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,15 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Employee;
+use App\Models\Task;
+use App\Models\Project;
 use DB;
 use Validator;
 use Auth;
-use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+        $task_count = Task::count();
+        $emp_count = Employee::count();
+        $prj_count = Project::count();
+
+        $prj_id = DB::table('projects')->first();
+        
+
+        $prjs = DB::table('projects')
+                    ->select('projects.*')
+                    ->selectRaw("COUNT(CASE WHEN tasks.status = 2 THEN 1 ELSE NULL END) as doneTask")
+                    ->selectRaw("COUNT(CASE WHEN tasks.status = 0 or tasks.status = 1 THEN 1 ELSE NULL END) AS leftTask")
+                    ->join('tasks','tasks.prj_id','=','projects.id')
+                    ->groupBy('projects.id')
+                    ->get();
+
+        return view('admin.admin_dashboard', compact('task_count', 'emp_count', 'prj_count', 'prjs'));
     }
 
     public function AllAdmin(){
